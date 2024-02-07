2122ba48d9eaea54c99df2c21ab7be646c09755b
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-02-06 21:18:00 -0800

    Add tests for ItemBox

---
+++ test/db/queries/ItemBox.spec.ts
@@ -0,0 +1,41 @@
+import ItemBoxQuery from "../../../src/db/queries/ItemBoxQuery";
+import * as TestItems from "../test_objs/ItemBox";
+import {testCreate, testDelete, testRead, testReadAll, testUpdate} from "./SimpleCrudQueryable";
+import {ItemBoxInitializer, ItemBoxMutator} from "../../../src/types/db/public/ItemBox";
+
+const testItemBoxInitializer: ItemBoxInitializer = {
+	item_id: 3,
+	quantity_per_box: 6
+};
+
+describe("ItemIndividual Query Tests", () => {
+
+	testCreate(ItemBoxQuery, {
+		testInitializer: testItemBoxInitializer,
+		getId: (q) => q.item_box_id
+	});
+
+	testRead(ItemBoxQuery, {
+		testId: TestItems.itemBoxClifBar.item_box_id,
+		testQueryable: TestItems.itemBoxClifBar,
+		nonexistentId: -1
+	});
+
+	testReadAll(ItemBoxQuery, Object.values(TestItems));
+
+	const itemBoxMutator: ItemBoxMutator = {
+		quantity_per_box: 10
+	};
+	testUpdate(ItemBoxQuery, {
+		testInitializer: testItemBoxInitializer,
+		testMutator: itemBoxMutator,
+		nonexistentId: -1,
+		getId: (q) => q.item_box_id
+	});
+
+	testDelete(ItemBoxQuery, {
+		testInitializer: testItemBoxInitializer,
+		nonexistentId: -1,
+		getId: (q) => q.item_box_id
+	});
+});
---
+++ test/db/test_objs/ItemBox.ts
@@ -0,0 +1,19 @@
+import ItemBox from "../../../src/types/db/public/ItemBox";
+
+export const itemBoxClifBar: ItemBox = {
+	item_box_id: 1,
+	item_id: 1,
+	quantity_per_box: 12
+};
+
+export const itemBoxRamen: ItemBox = {
+	item_box_id: 2,
+	item_id: 2,
+	quantity_per_box: 6
+};
+
+export const itemBoxCola: ItemBox = {
+	item_box_id: 3,
+	item_id: 3,
+	quantity_per_box: 12
+};
