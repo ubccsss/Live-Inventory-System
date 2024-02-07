c242bcd28087bdff26c2df04e8be0fd20cda6464
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-02-06 21:52:54 -0800

    Add ValidCategory tests

---
+++ test/db/queries/ValidCategory.spec.ts
@@ -0,0 +1,41 @@
+
+import * as TestItems from "../test_objs/ValidCategory";
+import {testCreate, testDelete, testRead, testReadAll, testUpdate} from "./SimpleCrudQueryable";
+import ValidCategoryQuery from "../../../src/db/queries/ValidCategoryQuery";
+import {ValidCategoryInitializer, ValidCategoryMutator} from "../../../src/types/db/public/ValidCategory";
+
+const testValidCategoryInitializer: ValidCategoryInitializer = {
+	category: "category"
+};
+
+describe("ItemIndividual Query Tests", () => {
+
+	testCreate(ValidCategoryQuery, {
+		testInitializer: testValidCategoryInitializer,
+		getId: (q) => q.category
+	});
+
+	testRead(ValidCategoryQuery, {
+		testId: TestItems.categoryDrink.category,
+		testQueryable: TestItems.categoryDrink,
+		nonexistentId: "fake"
+	});
+
+	testReadAll(ValidCategoryQuery, Object.values(TestItems));
+
+	const validCategoryMutator: ValidCategoryMutator = {
+		category: "wow"
+	};
+	testUpdate(ValidCategoryQuery, {
+		testInitializer: testValidCategoryInitializer,
+		testMutator: validCategoryMutator,
+		nonexistentId: "fake",
+		getId: (q) => q.category
+	});
+
+	testDelete(ValidCategoryQuery, {
+		testInitializer: testValidCategoryInitializer,
+		nonexistentId: "wow",
+		getId: (q) => q.category
+	});
+});
---
+++ test/db/test_objs/ValidCategory.ts
@@ -0,0 +1,17 @@
+import ValidCategory from "../../../src/types/db/public/ValidCategory";
+
+export const categoryFood: ValidCategory = {
+	category: "food"
+};
+
+export const categoryDrink: ValidCategory = {
+	category: "drink"
+};
+
+export const categoryMerch: ValidCategory = {
+	category: "merch"
+};
+
+export const categoryTicket: ValidCategory = {
+	category: "ticket"
+};
