e6dc69c134276cd7a66c1f1ca701aa45c57f904d
UBC Student <cailors@students.cs.ubc.ca>
2024-02-24 17:24:33 +0800

    use bootstrap components

--- frontend/src/components/tasklist.tsx
+++ frontend/src/components/tasklist.tsx
@@ -1,9 +1,7 @@
 import { memo } from 'react';
 import type { FC } from 'react';
-
-import resets from '../styles/_resets.module.css';
-import { TasklistButton } from './tasklistButton';
-import classes from '../styles/Tasklist.module.css';
+import '../styles/tasklist.css';
+import { Row, Button, Container, Col, Stack } from 'react-bootstrap';
 
 interface Props {
   className?: string;
@@ -11,23 +9,23 @@ interface Props {
 /* @figmaId 55:4 */
 export const Tasklist: FC<Props> = memo(function Screen(props = {}) {
   return (
-    <div className={`${resets.storybrainResets} ${classes.root}`}>
-      <div className={classes.background}></div>
-      <div className={classes.button3}></div>
-      <div className={classes.reimbursementRequest}>Reimbursement Request</div>
-      <div className={classes.button32}></div>
-      <div className={classes.transactionLog}>Transaction History </div>
-      <div className={classes.finance}>Finance</div>
-      <div className={classes.button33}>
-        <TasklistButton className={classes.icon} />
-      </div>
-      <div className={classes.getShoppingList}>Get Shopping List</div>
-      <div className={classes.button34}></div>
-      <div className={classes.updateInventory}>View/Update Inventory</div>
-      <div className={classes.manageInventory}>Manage Inventory</div>
-      <div className={classes.button35}></div>
-      <div className={classes.reimbursementRequest2}>Logout</div>
-    </div>
+	<div className='background'>
+		<Stack gap={3} className='tasklist'>
+			<div className="manageInventory">Manage Inventory</div>
+			<div className="viewUpdateInventory">
+				<Button className='viewUpdateInventoryButton' type="button" aria-pressed="true">
+					<a href='./'>View/Update Inventory</a>
+				</Button>
+			</div>
+			<div className="getShoppingList">
+				<Button className='getShoppingListButton' type="button" aria-pressed="true">
+					<a href='./'>Get Shopping List</a>
+				</Button>
+			</div>
+		
+		</Stack>
+	</div>
+    
   );
 });
 
