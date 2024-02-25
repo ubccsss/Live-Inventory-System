16685b2edeb6eb6a1cbc1c1ff6d9dd0725a48033
UBC Student <cailors@outlook.com>
2024-02-25 20:41:02 +0800

    finish tasklist page v1

--- frontend/src/components/tasklist.tsx
+++ frontend/src/components/tasklist.tsx
@@ -6,23 +6,38 @@ import { Row, Button, Container, Col, Stack } from 'react-bootstrap';
 interface Props {
   className?: string;
 }
-/* @figmaId 55:4 */
+
 export const Tasklist: FC<Props> = memo(function Screen(props = {}) {
   return (
 	<div className='background'>
 		<Stack gap={3} className='tasklist'>
-			<div className="manageInventory">Manage Inventory</div>
+			<div className="text-fff-48px-OpenSans manageInventory">Manage Inventory</div>
 			<div className="viewUpdateInventory">
-				<Button className='viewUpdateInventoryButton' type="button" aria-pressed="true">
+				<Button className='button-d9c4e3-24px-OpenSans' type="button" aria-pressed="true" >
 					<a href='./'>View/Update Inventory</a>
 				</Button>
 			</div>
 			<div className="getShoppingList">
-				<Button className='getShoppingListButton' type="button" aria-pressed="true">
+				<Button className='button-d9c4e3-24px-OpenSans' type="button" aria-pressed="true">
 					<a href='./'>Get Shopping List</a>
 				</Button>
 			</div>
-		
+			<div className="text-fff-48px-OpenSans finance">Finance</div>
+			<div className="transactionHistory">
+				<Button className='button-d9c4e3-24px-OpenSans' type="button" aria-pressed="true">
+					<a href='./'>Transaction History</a>
+				</Button>
+			</div>
+			<div className="reimbursementRequest">
+				<Button className='button-d9c4e3-24px-OpenSans' type="button" aria-pressed="true">
+					<a href='./'>Reimbursement Request</a>
+				</Button>
+			</div>
+			<div className="logout">
+				<Button className='button-d9c4e3-24px-OpenSans' type="button" aria-pressed="true">
+					<a href='./'>Logout</a>
+				</Button>
+			</div>
 		</Stack>
 	</div>
     
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
     extend: {},
