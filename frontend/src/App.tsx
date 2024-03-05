4d28028f69a933b76f4a61936db63a7b143a72ad
sl-81 <s_li1992@hotmail.com>
2024-03-05 15:06:32 -0800

    Update Navbar to support pages without title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,72 +1,109 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import ShoppingList from "./components/ShoppingList";
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Navbar
-              title="Catalogue"
-              needbuttons={false}
-              isofficerpage={false}
-            />
-          }
-        />
+        <Route path="/" element={<ShoppingList />} />
         <Route
           path="/store"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={false} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={false}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer"
           element={
-            <Navbar title="Login" needbuttons={false} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={false}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer/mainmenu"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer/itemdetail/:id"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer/reimburse"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer/inventory"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer/shoppinglist"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer/additem"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
         <Route
           path="/officer/transaction"
           element={
-            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+            <Navbar
+              title="Login"
+              needbuttons={true}
+              isofficerpage={true}
+              needtitle={true}
+            />
           }
         />
       </Routes>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,14 +4,20 @@ interface NavbarProps {
   title: string;
   needbuttons: boolean;
   isofficerpage: boolean;
+  needtitle: boolean;
 }
 
-const Navbar = ({ title, needbuttons, isofficerpage }: NavbarProps) => {
+const Navbar = ({
+  title,
+  needbuttons,
+  isofficerpage,
+  needtitle,
+}: NavbarProps) => {
   return (
     <div className={isofficerpage ? "darkmode" : "lightmode"}>
       {needbuttons && (
         <div className="flex justify-end right-4 p-4 text-black-700">
-          <Link to="/officer/mainmenu" className="btn-o">
+          <Link to="/officer/mainmenu" className="btn-o mr-4">
             Back to Main
           </Link>
 
@@ -20,14 +26,18 @@ const Navbar = ({ title, needbuttons, isofficerpage }: NavbarProps) => {
           </Link>
         </div>
       )}
-      <div className="relative top-15 text-center text-6xl ">{title}</div>
-      <hr
-        className={
-          isofficerpage
-            ? "h-px my-6 bg-white border-1 w-4/5 mx-auto"
-            : "h-px my-6 bg-black border-1 w-4/5 mx-auto"
-        }
-      />
+      {needtitle && (
+        <div className="relative top-15 text-center text-6xl ">
+          {title}
+          <hr
+            className={
+              isofficerpage
+                ? "h-px my-6 bg-white border-1 w-4/5 mx-auto"
+                : "h-px my-6 bg-black border-1 w-4/5 mx-auto"
+            }
+          />
+        </div>
+      )}
     </div>
   );
 };
