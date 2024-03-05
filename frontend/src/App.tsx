53f336ed25154426390ba8f9e34a3150ec7ba71f
sl-81 <s_li1992@hotmail.com>
2024-03-05 14:42:44 -0800

    updated index.css to include cust colours and changed Navbar to be reusable between o and c

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,39 +9,57 @@ const App = () => {
         <Route path="/" element={<ShoppingList />} />
         <Route
           path="/store"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={false} />
+          }
         />
         <Route
           path="/officer"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
         <Route
           path="/officer/mainmenu"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
         <Route
           path="/officer/itemdetail/:id"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
         <Route
           path="/officer/reimburse"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
         <Route
           path="/officer/inventory"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
         <Route
           path="/officer/shoppinglist"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
         <Route
           path="/officer/additem"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
         <Route
           path="/officer/transaction"
-          element={<Navbar title="Login" needbuttons={true} />}
+          element={
+            <Navbar title="Login" needbuttons={true} isofficerpage={true} />
+          }
         />
       </Routes>
     </Router>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,25 +3,27 @@ import { Link } from "react-router-dom";
 interface NavbarProps {
   title: string;
   needbuttons: boolean;
+  isofficerpage: boolean;
 }
 
-const Navbar = ({ title, needbuttons }: NavbarProps) => {
+const Navbar = ({ title, needbuttons, isofficerpage }: NavbarProps) => {
   return (
-    <div className="darkmode">
+    <div className={isofficerpage ? "darkmode" : "lightmode"}>
       {needbuttons && (
         <div className="flex justify-end right-4 p-4 text-black-700">
-          <Link to="/officer/mainmenu" className="btn mr-4">
+          <Link
+            to="/officer/mainmenu"
+            className={isofficerpage ? "btn-o mr-4" : "btn-c mr-4"}
+          >
             Back to Main
           </Link>
 
-          <Link to="/officer" className="btn">
+          <Link to="/officer" className={isofficerpage ? "btn-o" : "btn-c"}>
             Logout
           </Link>
         </div>
       )}
-      <div className="relative top-15 text-center text-6xl text-white">
-        {title}
-      </div>
+      <div className="relative top-15 text-center text-6xl ">{title}</div>
       <hr
         className="
             h-px 
